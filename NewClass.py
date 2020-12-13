6469425f85e77fd4adcd35e977d7655f517cc1f4
Maxim Verenin <maxverenin@mail.ru>
2020-12-13 19:04:38 +0300

    Last version

--- NewClass.py
+++ NewClass.py
@@ -1,10 +1,10 @@
 import pygame
 
 class Bullet:
-def __init__(self, coord):
-    self.coord = coord
-def Move (self, dy):
-    self.coord[1] += dy    
+    def __init__(self, coord):
+        self.coord = coord
+    def Move (self, dy):
+        self.coord[1] += dy    
 
 class Hero:     #создает класс персонажа (и доброго, и злого)
     def __init__(self, coord, max_health, is_good, Images, is_alive = True, is_protected = False):    #координаты, здоровье, максимальное здоровье, добрый или злой (True или False),
@@ -17,7 +17,7 @@ class Hero:     #создает класс персонажа (и доброго
         self.images = Images
         self.weapon = {"blaster" : 10,
                        'pg' : 1}
-       def __del__(self):    #деструктор
+    def __del__(self):    #деструктор
         del self.coord
         del self.health
         del self.max_health
@@ -29,19 +29,22 @@ class Hero:     #создает класс персонажа (и доброго
     def is_main(self):    #проверка: добрый или злой
         return is_good
     def blit_me(self, display_surface):    #рисует на экране
-        if self.is_main == True:
-            display_surface.blit(self.images[0],(self.coord[0],self.coord[1]))
+        if self.is_alive == True:
+            if self.is_main == True:
+                display_surface.blit(self.images[0],(self.coord[0],self.coord[1]))
+            else:
+                display_surface.blit(self.images[1],(self.coord[0],self.coord[1]))
         else:
-            display_surface.blit(self.images[1],(self.coord[0],self.coord[1]))
+            pass
     def Distruction(self):    #уничтожение
         self.is_alive = False
         #return self.is_alive
     def Status(self):    #текущий статус
         return self.is_alive
     def Move (self, dx, dy):    #перемещение
-        if (self.coord[0]>0)&(self.coord[0]<500):
+        if (self.coord[0]+dx>0)&(self.coord[0]+dx<500):
             self.coord[0] += dx
-        if (self.coord[0]>0)&(self.coord[0]<780):
+        if (self.coord[1]+dy>0)&(self.coord[1]+dy<780):
             self.coord[1] += dy
         #return self.coord
     def Doctor (self):    #использование аптечки
@@ -56,7 +59,7 @@ class Hero:     #создает класс персонажа (и доброго
             else:
                 self.is_alive = False
                 #return self.is_alive
-    def Shoot (self, keys)   #выстрел
+    def Shoot (self, keys):   #выстрел
         if keys[pygame.K_1]:
             curr_weapon = self.weapon['blaster']
         if keys[pygame.K_2]:
